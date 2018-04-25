51a90922b637b05a2944b83d0c0799a68f3c18b9
Saúl Ponce <saul_enrique-7285-96@hotmail.com>
2018-04-24 21:55:58 -0500

    Add things

---
+++ index.js
@@ -0,0 +1,34 @@
+const http = require('http');
+const https = require('https');
+
+const handler = (req, res) => {
+  res.end('Hello World!');
+}
+
+const PROD = false;
+const lex = require('greenlock-express').create({
+  version: 'v02',
+  server: PROD ? 'https://acme-v01.api.letsencrypt.org/directory' : 'staging',
+ 
+  approveDomains: (opts, certs, cb) => {
+    if (certs) {
+      // change domain list here
+      opts.domains = ['blindcon.galaxiaskyklos.com']
+    } else { 
+      // change default email to accept agreement
+      opts.email = 'saul_enrique-7285-96@hotmail.com'; 
+      opts.agreeTos = true;
+    }
+    cb(null, { options: opts, certs: certs });
+  }
+});
+
+const middlewareWrapper = lex.middleware;
+
+https.createServer(
+  lex.httpsOptions, 
+  middlewareWrapper(handler)
+).listen(433);
+
+const redirectHttps = require('redirect-https');
+http.createServer(lex.middleware(redirectHttps())).listen(80);
