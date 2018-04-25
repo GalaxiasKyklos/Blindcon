d20a91aa69c04a0d8239a3c33ce14a02cecb6d2c
Saúl Ponce <saul_enrique-7285-96@hotmail.com>
2018-04-25 11:13:43 -0500

    Try https

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require('express')
 const proxy = require('http-proxy-middleware')
+const https = require('https')
+const fs = require('fs');
 
 const app = express()
+
+const key = fs.readFileSync('/etc/letsencrypt/live/blindcon.galaxiaskyklos.com/privkey.pem', 'utf8')
+const cert = fs.readFileSync('/etc/letsencrypt/live/blindcon.galaxiaskyklos.com/cert.pem', 'utf8')
+const ca = fs.readFileSync('/etc/letsencrypt/live/blindcon.galaxiaskyklos.com/chain.pem', 'utf8')
+
+const credentials = {
+	key,
+	cert,
+	ca,
+}
 
 const apiProxy = proxy('/api', {
   target: 'http://localhost:3001',
@@ -18,7 +30,13 @@ app.use(express.static(__dirname, {
 app.use(apiProxy)
 app.use(frontProxy)
 
+const httpsServer = https.createServer(credentials, app)
+const httpServer = http.createServer(app)
 
-app.listen(80, () => {
-  console.log('HTTP server running on port 80')
+httpServer.listen(80, () => {
+	console.log('HTTPS Server running on port 80');
+})
+
+httpsServer.listen(443, () => {
+	console.log('HTTPS Server running on port 443');
 })
