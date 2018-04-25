d6c3813d2248920f12f4b611b183dd9193eb9f60
Saúl Ponce <saul_enrique-7285-96@hotmail.com>
2018-04-25 10:47:19 -0500

    Use a better proxy

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
 const express = require('express')
-const request = require('request')
+const proxy = require('http-proxy-middleware')
 
 const app = express()
 
+const apiProxy = proxy('/api', {
+  target: 'http://localhost:3001',
+})
+
+const frontProxy = proxy('/', {
+  target: 'http://localhost:3000',
+})
+
+app.use(apiProxy)
+app.use(frontProxy)
+
 app.use(express.static(__dirname, {
-  dotfiles: 'allow'
+  dotfiles: 'allow',
 }))
 
 app.listen(80, () => {
   console.log('HTTP server running on port 80')
-})
-
-app.use('/', (req, res) => {
-  const port = req.url.includes('api') ? 3001 : 3000
-  const url = `http://localhost:${port}${req.url}`
-  let r = null
-  if (req.method === 'POST') {
-    r = request.post({
-      uri: url,
-      json: req.body
-    })
-  } else {
-    r = request(url)
-  }
-  req.pipe(r).pipe(res)
 })
